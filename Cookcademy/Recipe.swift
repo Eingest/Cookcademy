7bb499d51c074779988344a8ca519554562df9b5
Eingest <ak0805@live.de>
2022-04-17 19:15:02 +0200

    added the recipe.swift file for cooking

---
+++ Cookcademy/Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  Cookcademy
+//
+//  Created by Andreas Kiesel on 17.04.22.
+//
+
+import Foundation
+
+struct Recipe {
+    var name: String
+    var description: String
+    var author: String
+    var category: String
+    var ingredients: [String]
+    var directions: [String]
+}
