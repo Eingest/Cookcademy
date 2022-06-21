4425128ec924787668d9cd4a366b8fe3e4612da8
Eingest <ak0805@live.de>
2022-06-21 16:23:40 +0200

    loading and saving - finish

--- Cookcademy/ViewModels/RecipeData.swift
+++ Cookcademy/ViewModels/RecipeData.swift
@@ -10,8 +10,39 @@ import Foundation
 class RecipeData: ObservableObject {
     @Published var recipes = Recipe.testRecipes
     
+    private var recipesFileURL: URL {
+        do {
+            let documentsDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            return documentsDirectory.appendingPathComponent("recipeData")
+        }
+        catch {
+            fatalError("An error occured while getting the url: \(error)")
+        }
+    }
+    
     var favoriteRecipes: [Recipe] {
         recipes.filter { $0.isFavorite }
+    }
+    
+    func saveRecipes() {
+        do {
+            let encodedData = try JSONEncoder().encode(recipes)
+            try encodedData.write(to: recipesFileURL)
+        }
+        catch {
+            fatalError("An error occured while saving recipes: \(error)")
+        }
+    }
+    
+    func loadRecipes() {
+        guard let data = try? Data(contentsOf: recipesFileURL) else { return }
+        do {
+            let savedRecipes = try JSONDecoder().decode([Recipe].self, from: data)
+            recipes = savedRecipes
+        }
+        catch {
+            fatalError("An error occured while loading recipes: \(error)")
+        }
     }
     
     func recipes(for category: MainInformation.Category) -> [Recipe] {
@@ -27,6 +58,7 @@ class RecipeData: ObservableObject {
     func add(recipe: Recipe) {
         if recipe.isValid {
             recipes.append(recipe)
+            saveRecipes()
         }
     }
     
